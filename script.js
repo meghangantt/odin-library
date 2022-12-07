e393f3ef7ce48105bd9265dd5029a265f57c0ced
meghangantt <meghangantt@gmail.com>
2022-12-07 15:37:59 -0500

    Enable user input to library through input form

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const testBook1 = new Book('Author1', 'Book1', '41', true);
 const testBook2 = new Book('Author2', 'Book2', '60', false);
 const library = document.querySelector('.library');
 const newBookBtn = document.querySelector('.new-book-btn');
+const submitFormBtn = document.querySelector('.submit-btn');
+const inputForm = document.querySelector('.input-form')
 
 let myLibrary = [testBook1, testBook2];
 
@@ -9,12 +11,18 @@ function Book(author, title, pages, read) {
     this.author = author;
     this.title = title;
     this.pages = pages;
-    this.read = read
+    this.read = read;
 }
 
-function addBookToLibrary() {
+function addBookToLibrary(e) {
+    e.preventDefault();
+    let author = document.getElementById("author").value;
+    let title = document.getElementById("title").value;
+    let pages = document.getElementById("pages").value;
+    let read = document.getElementById("read").value;
     const newBook = new Book(author, title, pages, read);
     myLibrary.push(newBook);
+    closeTheForm();
 }
 
 function displayBooks() {
@@ -35,5 +43,7 @@ function closeTheForm() {
 function openTheForm() {
     document.getElementById("input-popup").style.display = "block";
 }
+
+inputForm.addEventListener("submit", addBookToLibrary);
 
 closeTheForm();
